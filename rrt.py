e112228badf70207963d9acd95b29df6ed56e5ff
mamoon <sheikh.mamoon.mondal@gmail.com>
2020-07-21 23:22:00 +0530

    finished adding comments ... finalized..done

--- rrt.py
+++ rrt.py
@@ -1,11 +1,17 @@
+'''
+SAMPLING-BASED PLANNING
+
+SK MOHAMMED MAMOON MONDAL
+
+'''
+
 import numpy as np
 import random
 
-import matplotlib.pyplot as plt
-import math
-
-
+#list to store address of the objects of tree_rrt class
 nodes=[]
+
+#Data structure to store nodes
 class tree_rrt:
     tree_size=0
     def __init__(self,x,y):
@@ -15,15 +21,8 @@ class tree_rrt:
         tree_rrt.tree_size+=1
         self.index=tree_rrt.tree_size
         print(self.index)
-'''
-def get_random_node(self):
-        if random.randint(0, 100) > self.goal_sample_rate:
-            rnd = self.Node(random.uniform(self.min_rand, self.max_rand),
-                            random.uniform(self.min_rand, self.max_rand))
-        else:  # goal point sampling
-            rnd = self.Node(self.end.x, self.end.y)
-        return rnd
-'''
+
+#finds the nearest node to the sample node
 def nearest_node(sample):
     dist=999999
     index=0
@@ -34,6 +33,7 @@ def nearest_node(sample):
             index=i.index
     return [dist,index]
 
+#checks for collision
 def collision(local,obstacles):
     collide=False
     for item in local:
@@ -43,7 +43,7 @@ def collision(local,obstacles):
                 return collide
     return collide
 
-
+#returns the path at the end after the goal is reached
 def retrive_path():
     path=[]
     i=tree_rrt.tree_size
@@ -57,37 +57,37 @@ def retrive_path():
     path.append(i)
     return path
 
+#returns euclidean distance between two points
 def check_distance(p1,p2):
     return np.sqrt(np.square(p1[0]-p2[0])+np.square(p1[1]-p2[1]))
 
+
+#function to implement the rrt sampling based path finding algorithm
 def rrt(obstacles,start,goal,max_size,scale=1):
-    #nodes=[]
-    animation=False
+
     edges=[]
     path=[]
-    dmin=0.05*scale
+    dmin=0.1*scale
     nodes.append(tree_rrt(start[0],start[1]))
     while tree_rrt.tree_size < max_size:
         x_sample=random.uniform(start[0]-0*scale,goal[0]+0.02*scale)
         y_sample=random.uniform(start[1]-0*scale,goal[1]+0.02*scale)
-        #x_sample=random.uniform(nodes[int(tree_rrt.tree_size-1)].x,goal[0]+0.1*scale)
-        #y_sample=random.uniform(nodes[int(tree_rrt.tree_size-1)].y,goal[1]+0.1*scale)
+
         sample=[x_sample,y_sample]
         edge=nearest_node(sample)
         if edge[0]>(dmin*2):
             continue
-        #step=(nodes[int(edge[1]-1)].x-sample[0])/100)
-        local_x=[i for i in np.linspace(nodes[int(edge[1]-1)].x,sample[0],5)]
-        local_y=[i for i in np.linspace(nodes[int(edge[1]-1)].y,sample[1],5)]
+
+        local_x=[i for i in np.linspace(nodes[int(edge[1]-1)].x,sample[0],5)]#creates a list of straight line points between nearest node and sample
+        local_y=[i for i in np.linspace(nodes[int(edge[1]-1)].y,sample[1],5)]#creates a list of straight line points between nearest node and sample
         local=zip(local_x,local_y)
-        #print('local',local)
+
         if collision(local,obstacles):
             continue
         nodes.append(tree_rrt(x_sample,y_sample))
         nodes[int(tree_rrt.tree_size-1)].parent=edge[1]
         edges.append([edge[1],nodes[int(tree_rrt.tree_size-1)].index,edge[0]])
-        if animation and tree_rrt.tree_size % 5 == 0:
-            draw_graph(edge,obstacles)
+
         if check_distance(goal,sample)<dmin:
             print("found something")
             local_x=[i for i in np.linspace(goal[0],sample[0],5)]
@@ -100,49 +100,17 @@ def rrt(obstacles,start,goal,max_size,scale=1):
             edges.append([nodes[int(tree_rrt.tree_size-2)].index,tree_rrt.tree_size-1,check_distance(goal,sample)])
             path=retrive_path()
             break
-        if animation and tree_rrt.tree_size % 5 == 0:
-            draw_graph(edge,obstacles)
+
 
     nodes1=[]
-    #print(edges)
     for j in nodes:
         nodes1.append([j.index,j.x,j.y])
         print([j.x,j.y])
     return [path[::-1],nodes1,edges]
 
 
-
-def plot_circle(x, y, size, color="-b"):  # pragma: no cover
-    size=size/2
-    deg = list(range(0, 360, 5))
-    deg.append(0)
-    xl = [x + size * math.cos(np.deg2rad(d)) for d in deg]
-    yl = [y + size * math.sin(np.deg2rad(d)) for d in deg]
-    plt.plot(xl, yl, color)
-
-def draw_graph(rnd,obstacle_list):
-    plt.clf()
-    # for stopping simulation with the esc key.
-    plt.gcf().canvas.mpl_connect('key_release_event',
-                                 lambda event: [exit(0) if event.key == 'escape' else None])
-    if rnd is not None:
-        plt.plot(rnd[0], rnd[1], "^k")
-    for node in nodes:
-        if node.parent:
-            plt.plot(node.x, node.y, "-g")
-
-    for (ox, oy, size) in obstacle_list:
-        plot_circle(ox, oy, size)
-
-    plt.plot(start_node[0],start_node[1], "xr")
-    plt.plot(goal_node[0],goal_node[1], "xr")
-    plt.axis("equal")
-    plt.axis([-0.5,0.6,-0.5,0.6])
-    plt.grid(True)
-    plt.pause(0.01)
-
-
-obstacle_list=np.genfromtxt("obstacles.csv",delimiter=',')
+#calls the rrt function and creates obstacle list from csv file
+obstacle_list=np.genfromtxt("obstacles.csv",delimiter=',')#reads csv file
 scale=1
 start_node=[-0.5*scale,-0.5*scale]
 goal_node=[0.5*scale,0.5*scale]
@@ -155,6 +123,6 @@ else:
     edges=np.array(avenue[2],float)
     path=np.array(avenue[0],int)
     path=np.reshape(path,(1,path.size))
-    np.savetxt("nodes.csv",nodes1,delimiter=',')
-    np.savetxt("edges.csv",edges,delimiter=',')
-    np.savetxt("path.csv",path,delimiter=',')
+    np.savetxt("nodes.csv",nodes1,delimiter=',')#creates csv file
+    np.savetxt("edges.csv",edges,delimiter=',')#creates csv file
+    np.savetxt("path.csv",path,delimiter=',')#creates csv file
